09cd8a147b1637bdb28f942bb3ae52f78b07249c
cracker01a <sikpeabel6@gmail.com>
2024-07-29 09:59:59 +0100

    update notification alert

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -2,83 +2,75 @@
 
 @section('content')
 
-    <div class="nk-content ">
-        <div class="container-fluid">
-            <div class="nk-content-inner">
-                <div class="nk-content-body">
 
-                    @include('components.header',[
-                        'title' => "Modification du produit « ".$product->name." »",
-                        'back'   => [
-                            'label' => 'Liste des produits',
-                            'url'   => route('product.index'),
-                        ],
-                    ])
+    @include('components.header',[
+        'title' => "Modification du produit « ".$product->name." »",
+        'back'   => [
+            'label' => 'Liste des produits',
+            'url'   => route('product.index'),
+        ],
+    ])
 
-                    <div class="card">
-                        <div class="card-body">
+    <div class="card">
+        <div class="card-body">
 
-                            <form action="{{ route('product.update' , $product) }}" method="POST" id="create-form">
+            <form action="{{ route('product.update' , $product) }}" method="POST" id="create-form">
 
-                                @csrf
-                                @method('PUT')
+                @csrf
+                @method('PUT')
 
 
-                                <div class="row py-3">
+                <div class="row py-3">
 
-                                    <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label class="form-label" for="name">Nom</label>
-                                            <div class="form-control-wrap">
-                                                <input type="text"
-                                                        class="form-control @error('name') is-invalid @enderror"
-                                                        id="name"
-                                                        name="name"
-                                                        value="{{ $product->name }}"
-                                                        placeholder="Ex : Lampe">
-                                                @error('name')
-                                                    <span class="error">{{ $message }}</span>
-                                                @enderror
-                                            </div>
-                                        </div>
-                                    </div>
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label class="form-label" for="name">Nom</label>
+                            <div class="form-control-wrap">
+                                <input type="text"
+                                        class="form-control @error('name') is-invalid @enderror"
+                                        id="name"
+                                        name="name"
+                                        value="{{ $product->name }}"
+                                        placeholder="Ex : Lampe">
+                                @error('name')
+                                    <span class="error">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
 
-                                    <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label class="form-label" for="price">Prix</label>
-                                            <div class="form-control-wrap">
-                                                <input type="number"
-                                                        class="form-control @error('price') is-invalid @enderror"
-                                                        id="price"
-                                                        name="price"
-                                                        value="{{ $product->price }}"
-                                                        placeholder="Ex : 5 000">
-                                                @error('price')
-                                                    <span class="error">{{ $message }}</span>
-                                                @enderror
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                </div>
-
-                                <div class="pt-3 text-center">
-                                    <button type="submit"
-                                            class="btn btn-md btn-primary"
-                                            title="Modifier un produit">
-                                        Modifier
-                                    </button>
-                                </div>
-
-                            </form>
-
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label class="form-label" for="price">Prix</label>
+                            <div class="form-control-wrap">
+                                <input type="number"
+                                        class="form-control @error('price') is-invalid @enderror"
+                                        id="price"
+                                        name="price"
+                                        value="{{ $product->price }}"
+                                        placeholder="Ex : 5 000">
+                                @error('price')
+                                    <span class="error">{{ $message }}</span>
+                                @enderror
+                            </div>
                         </div>
                     </div>
 
                 </div>
-            </div>
+
+                <div class="pt-3 text-center">
+                    <button type="submit"
+                            class="btn btn-md btn-primary"
+                            title="Modifier un produit">
+                        Modifier
+                    </button>
+                </div>
+
+            </form>
+
         </div>
     </div>
+
 
 @endsection
 
--- resources/views/site/edit.blade.php
+++ resources/views/site/edit.blade.php
@@ -2,65 +2,58 @@
 
 @section('content')
 
-    <div class="nk-content ">
-        <div class="container-fluid">
-            <div class="nk-content-inner">
-                <div class="nk-content-body">
 
-                    @include('components.header',[
-                        'title' => "Modification du site « ".$site->name." »",
-                        'back'   => [
-                            'label' => 'Liste des sites',
-                            'url'   => route('site.index'),
-                        ],
-                    ])
+    @include('components.header',[
+        'title' => "Modification du site « ".$site->name." »",
+        'back'   => [
+            'label' => 'Liste des sites',
+            'url'   => route('site.index'),
+        ],
+    ])
 
-                    <div class="card">
-                        <div class="card-body">
+    <div class="card">
+        <div class="card-body">
 
-                            <form action="{{ route('site.update' , $site) }}" method="POST" id="create-form">
+            <form action="{{ route('site.update' , $site) }}" method="POST" id="create-form">
 
-                                @csrf
-                                @method('PUT')
+                @csrf
+                @method('PUT')
 
 
-                                <div class="row py-3">
+                <div class="row py-3">
 
-                                    <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label class="form-label" for="name">Nom</label>
-                                            <div class="form-control-wrap">
-                                                <input type="text"
-                                                        class="form-control @error('name') is-invalid @enderror"
-                                                        id="name"
-                                                        name="name"
-                                                        value="{{ $site->name }}"
-                                                        placeholder="Ex : Lampe">
-                                                @error('name')
-                                                    <span class="error">{{ $message }}</span>
-                                                @enderror
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                </div>
-
-                                <div class="pt-3 text-center">
-                                    <button type="submit"
-                                            class="btn btn-md btn-primary">
-                                        Modifier
-                                    </button>
-                                </div>
-
-                            </form>
-
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label class="form-label" for="name">Nom</label>
+                            <div class="form-control-wrap">
+                                <input type="text"
+                                        class="form-control @error('name') is-invalid @enderror"
+                                        id="name"
+                                        name="name"
+                                        value="{{ $site->name }}"
+                                        placeholder="Ex : Lampe">
+                                @error('name')
+                                    <span class="error">{{ $message }}</span>
+                                @enderror
+                            </div>
                         </div>
                     </div>
 
                 </div>
-            </div>
+
+                <div class="pt-3 text-center">
+                    <button type="submit"
+                            class="btn btn-md btn-primary">
+                        Modifier
+                    </button>
+                </div>
+
+            </form>
+
         </div>
     </div>
+
+             
 
 @endsection
 
